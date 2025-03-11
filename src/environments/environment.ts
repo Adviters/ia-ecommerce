c6c2a9c1639f60d917114394a675b0b3d72a817d
jrios-adviters <jeronimo.rios@adviters.com>
2025-03-11 11:09:04 -0300

    fix footer

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   homeRequestUrl:
-    'https://0589-2800-810-565-248-5dcf-5ba8-492b-8dfa.ngrok-free.app/api/v1/ollama/market-all-products',
+    'https://3200-2800-810-565-248-4a23-d902-ca5d-98cc.ngrok-free.app/api/v1/ollama/market-all-products',
   detailRequestUrl:
-    'https://0589-2800-810-565-248-5dcf-5ba8-492b-8dfa.ngrok-free.app/api/v1/ollama/market-product',
+    'https://3200-2800-810-565-248-4a23-d902-ca5d-98cc.ngrok-free.app/api/v1/ollama/market-product',
   shoesListUrl:
-    'https://0589-2800-810-565-248-5dcf-5ba8-492b-8dfa.ngrok-free.app/api/v1/ollama/products',
+    'https://3200-2800-810-565-248-4a23-d902-ca5d-98cc.ngrok-free.app/api/v1/ollama/products',
 };
