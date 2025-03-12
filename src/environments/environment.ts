0453d5227f1ecdc485cdc5582a45ac8fe13174d3
iormaieru <iormaieru@hotmail.com>
2025-03-12 17:20:31 -0300

    enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,5 @@
 export const environment = {
-  homeRequestUrl:
-    'https://3200-2800-810-565-248-4a23-d902-ca5d-98cc.ngrok-free.app/api/v1/ollama/market-all-products',
-  detailRequestUrl:
-    'https://3200-2800-810-565-248-4a23-d902-ca5d-98cc.ngrok-free.app/api/v1/ollama/market-product',
-  shoesListUrl:
-    'https://3200-2800-810-565-248-4a23-d902-ca5d-98cc.ngrok-free.app/api/v1/ollama/products',
+  homeRequestUrl: 'http://0.0.0.0:5004/api/v1/ollama/market-all-products',
+  detailRequestUrl: 'http://0.0.0.0:5004/api/v1/ollama/market-product',
+  shoesListUrl: 'http://0.0.0.0:5004/api/v1/ollama/products',
 };
